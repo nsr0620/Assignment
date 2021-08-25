8b92d177b0731cfe18245202bd3d83c17dbcfcf7
nsr0620 <89535622+nsr0620@users.noreply.github.com>
2021-08-25 21:14:54 +0530

    Day 1 Assignmnent

---
+++ simple_calculator.js
@@ -0,0 +1,12 @@
+a = 60
+b = 12 
+
+sum = a + b
+diff = a - b
+mul = a*b 
+div = a/b
+
+console.log("Addition of two numbers = ",sum)
+console.log("Subtraction of two numbers = ",diff)
+console.log("Multiplication of two numbers = ",mul)
+console.log("Division of two numbers = ",div)
